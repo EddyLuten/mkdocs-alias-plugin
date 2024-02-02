4a4559ed9334e98f0a30808a412e0850080afd1d
Eddy Luten <eddyluten@gmail.com>
2024-02-01 18:49:54 -0600

    Bump plugin version and workflow action versions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='mkdocs-alias-plugin',
-    version='0.6.0',
+    version='0.7.0',
     description=
     'An MkDocs plugin allowing links to your pages using a custom alias',
     long_description=long_description,
