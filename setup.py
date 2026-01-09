06a21f085c3455f5b5dd5616431c0e7fe4031716
Eddy Luten <eluten@pm.me>
2026-01-08 21:13:23 -0600

    Forgot to bump the version. v0.10.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='mkdocs-alias-plugin',
-    version='0.10.0',
+    version='0.10.1',
     description=
     'An MkDocs plugin allowing links to your pages using a custom alias',
     long_description=long_description,
